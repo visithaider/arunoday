af0feb68a62b9b81d5e58a0c1c0c70de49699d82
aparna.chaudhary <aparna.chaudhary@00b03aad-ec82-a75e-9712-461e874d0905>
2010-05-14 11:12:08 +0000

    Initial Import

---
+++ trunk/jpa2-sample/src/main/java/net/arunoday/repository/jpa/AbstractJpaReadRepository.java
@@ -0,0 +1,64 @@
+/**
+ * <TODO: Add Copyright>
+ *
+ * FILE        : AbstractJpaReadDao.java
+ *
+ * PACKAGE     : demo.repository.jpa
+ *
+ * CREATION DT : May 14, 2010
+ */
+
+package net.arunoday.repository.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import net.arunoday.model.BasicEntity;
+import net.arunoday.repository.ReadRepository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * A base class for all JPA based read repository implementations.
+ * 
+ * @author Aparna Chaudhary ( aparna.chaudhary@gmail.com )
+ */
+@Repository
+public abstract class AbstractJpaReadRepository<T extends BasicEntity> implements ReadRepository<T> {
+
+    private final Class<T> clazz;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * Constructs a new AbstractJpaReadRepository with a given entity class.
+     * 
+     * @param clazz The class of the entity this repository manages.
+     */
+    protected AbstractJpaReadRepository(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    public T getById(long id) {
+        return entityManager.find(clazz, id);
+    }
+
+    protected final Class<T> getEntityClass() {
+        return clazz;
+    }
+
+    public final EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("unchecked")
+    public List<T> findAll() {
+        return getEntityManager().createQuery("select e from " + getEntityClass().getName() + " e").getResultList();
+    }
+}
---
+++ trunk/jpa2-sample/src/main/java/net/arunoday/repository/jpa/AbstractJpaReadWriteRepository.java
@@ -0,0 +1,64 @@
+/**
+ * <TODO: Add Copyright>
+ *
+ * FILE        : AbstractJpaDao.java
+ *
+ * PACKAGE     : demo.repository.jpa
+ *
+ * CREATION DT : May 14, 2010
+ */
+
+package net.arunoday.repository.jpa;
+
+import net.arunoday.model.BasicEntity;
+import net.arunoday.repository.ReadWriteRepository;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Base class for all JPA based dao implementations.
+ * 
+ * @author Aparna Chaudhary ( aparna.chaudhary@gmail.com )
+ */
+@Repository
+public abstract class AbstractJpaReadWriteRepository<T extends BasicEntity> extends AbstractJpaReadRepository<T>
+        implements ReadWriteRepository<T> {
+
+    /**
+     * Constructs a new AbstractJpaReadWriteRepository with a given entity class.
+     * 
+     * @param clazz The class of the entity this repository manages.
+     */
+    protected AbstractJpaReadWriteRepository(Class<T> clazz) {
+        super(clazz);
+    }
+
+    /**
+     * @see ReadWriteRepository#save(Object)
+     */
+    public long save(T t) {
+        getEntityManager().persist(t);
+        getEntityManager().flush();
+        return t.getId();
+    }
+
+    /**
+     * @see ReadWriteRepository#update(Object)
+     */
+    public T update(T t) {
+        return getEntityManager().merge(t);
+    }
+
+    /**
+     * @see ReadWriteRepository#removeById(long)
+     */
+    public final T removeById(long id) {
+        T entity = getById(id);
+        if (entity == null) {
+            return null;
+        }
+        getEntityManager().remove(entity);
+        return entity;
+    }
+
+}
