fc6cfdf420b043b2144121230f0a4088fcee2a7b
aparna.chaudhary <aparna.chaudhary@00b03aad-ec82-a75e-9712-461e874d0905>
2010-12-01 16:28:29 +0000

    Upgraded to 5.0 GA release.

--- trunk/activiti-si-demo/src/main/java/net/arunoday/activiti/demo/handler/CheckAssignee.java
+++ trunk/activiti-si-demo/src/main/java/net/arunoday/activiti/demo/handler/CheckAssignee.java
@@ -8,7 +8,7 @@ import net.arunoday.activiti.demo.service.DefectService;
 import net.arunoday.activiti.demo.util.SpringApplicationContextUtils;
 
 import org.activiti.engine.delegate.DelegateExecution;
-import org.activiti.engine.delegate.JavaDelegation;
+import org.activiti.engine.delegate.JavaDelegate;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -18,23 +18,21 @@ import org.springframework.stereotype.Component;
  * @author Aparna Chaudhary
  */
 @Component("checkAssignee")
-public class CheckAssignee implements JavaDelegation {
+public class CheckAssignee implements JavaDelegate {
 
-	private static final Logger logger = Logger.getLogger(CheckAssignee.class);
+    private static final Logger logger = Logger.getLogger(CheckAssignee.class);
 
-	@Override
-	public void execute(DelegateExecution execution) throws Exception {
-		ApplicationContext context = SpringApplicationContextUtils
-				.getApplicationContext();
-		DefectService defectService = context.getBean(DefectService.class);
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        ApplicationContext context = SpringApplicationContextUtils.getApplicationContext();
+        DefectService defectService = context.getBean(DefectService.class);
 
-		Long defectId = (Long) execution.getVariable("defectId");
-		Defect defect = defectService.getDefectById(defectId);
-		
-		logger.info("Defect Id: " + defectId + "Assigned To: "
-				+ defect.getAssignedTo());
+        Long defectId = (Long) execution.getVariable("defectId");
+        Defect defect = defectService.getDefectById(defectId);
 
-		execution.setVariable("assignee", defect.getAssignedTo());
-	}
+        logger.info("Defect Id: " + defectId + "Assigned To: " + defect.getAssignedTo());
+
+        execution.setVariable("assignee", defect.getAssignedTo());
+    }
 
 }
